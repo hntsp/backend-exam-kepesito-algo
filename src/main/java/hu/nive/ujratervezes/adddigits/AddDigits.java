544386d830899fbb75d3346502f4047204fe0432
hntsp <hantosp@protonmail.com>
2021-04-29 10:27:57 +0200

    Update AddDigits.java

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,23 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+   public static int addDigits(String input) {
+
+       if (input == null || input.equals("")) return -1;
+
+       int l = input.length();
+       int sum = 0;
+       for (int i = 0; i < l; i++)
+       {
+           if (Character.isDigit(input.charAt(i)))
+           {
+               String temp = input.substring(i,i+1);
+               sum += Integer.parseInt(temp);
+           }
+       }
+
+       return sum;
+
+    }
 }
